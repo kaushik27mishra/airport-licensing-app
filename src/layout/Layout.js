3a2748f58c535402057e7ebff4a19998586aeeca
priyanshisharma <priyanshi417@gmail.com>
2020-07-06 18:14:32 +0530

    Added form3

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -14,6 +14,7 @@ import Dashboard from "../pages/dashboard/Dashboard"
 import SampleForm from "../pages/forms/SampleForm";
 import Form1 from "../pages/forms/form1/Form1"
 import Form2 from "../pages/forms/form2/Form2"
+import Form3 from "../pages/forms/form3/Form3";
 
 function Layout(props) {
 
@@ -31,8 +32,9 @@ function Layout(props) {
                 <Switch>
                   <Route path="/app/dashboard" component={Dashboard} />
                   <Route path="/app/sampleform" component={SampleForm} />
-                  <Route path="/app/licensee_detail_form" component={Form1} />
-                  <Route path="/app/aerodrome_detail_form" component={Form2} />
+                  <Route path="/app/licensee_detail_form" component={Form1}/>
+                  <Route path="/app/aerodrome_detail_form" component={Form2}/>
+                  <Route path="/app/aerodrome_activities" component={Form3}/>
                 </Switch>
               </div>
             </div>
