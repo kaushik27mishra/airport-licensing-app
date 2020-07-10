f4f10b907d7d34993321d295b2668011bfc12ed8
roma <romavofficial@gmail.com>
2020-07-10 19:54:23 +0530

    Forms :  6,7,8,9,renewal added

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -16,7 +16,11 @@ import Form1 from "../pages/forms/form1/Form1"
 import Form2 from "../pages/forms/form2/Form2"
 import Form3 from "../pages/forms/form3/Form3";
 import Form4 from "../pages/forms/form4/Form4";
-
+import Form6 from "../pages/forms/form6/Form6";
+import Form7 from "../pages/forms/form7/Form7";
+import Form8 from "../pages/forms/form8/Form8";
+import Form9 from "../pages/forms/form9/Form9";
+import RenewalForm from "../pages/forms/renewal/RenewalForm";
 function Layout(props) {
 
   return (
@@ -37,6 +41,11 @@ function Layout(props) {
                   <Route path="/app/aerodrome_detail_form" component={Form2}/>
                   <Route path="/app/aerodrome_activities" component={Form3}/>
                   <Route path="/app/control_of_aerodrome" component={Form4}/>
+                  <Route path="/app/aerodrome_management_personnel" component={Form6}/>
+                  <Route path="/app/aerodrome_manual" component={Form7}/>
+                  <Route path="/app/details_of_fees" component={Form8}/>
+                  <Route path="/app/further_info" component={Form9}/>
+                  <Route path="/app/renewal_form" component={RenewalForm}/>
                 </Switch>
               </div>
             </div>
