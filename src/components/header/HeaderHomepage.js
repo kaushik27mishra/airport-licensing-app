fae5b087bc2f557ed5599eb5e95df98919b175ef
priyanshisharma <priyanshi417@gmail.com>
2020-07-22 01:49:42 +0530

    Added Faqs

--- src/components/header/HeaderHomepage.js
+++ src/components/header/HeaderHomepage.js
@@ -3,10 +3,12 @@ import React from 'react';
 //components
 import { Link } from 'react-router-dom';
 //ui
-import { Text } from 'office-ui-fabric-react';
+import { Text , Stack} from 'office-ui-fabric-react';
 
 // styles
 import './style.css'
+
+const stackTokens = { childrenGap: 15 };
 
 function HeaderHomepage() {
     return (
@@ -14,7 +16,10 @@ function HeaderHomepage() {
             <div style={{height:'75px',padding:'0em',margin:'0',display:'flex',flexDirection:'row',backgroundColor:'#FFFFFF'}}>
                 <Text style={{marginTop:'10px',marginLeft:'40px',height:'100%',fontSize:'35px'}} variant={'medium'} nowrap>Airport Authority of India</Text>
                 <div style={{marginLeft:'67%',paddingTop:"20px"}}>
-                    <Link to='/login'>Login</Link>
+                    <Stack horizontal tokens={stackTokens}>
+                        <Link to='/faq'>FAQs</Link>
+                        <Link to='/login'>Login</Link>
+                    </Stack>
                 </div>
             </div>
         </div>
---
+++ src/pages/faq/Faq.js
@@ -0,0 +1,178 @@
+import React from 'react'
+
+//components
+import HeaderHomePage from '../../components/header/HeaderHomepage'
+//ui
+import { Text} from 'office-ui-fabric-react';
+import { Card } from '@uifabric/react-cards';
+import { mergeStyleSets } from 'office-ui-fabric-react/lib/Styling';
+
+//style
+import './style.css'
+
+const styles = {
+    cardStyles: {
+        root: {
+          background: 'white',
+          paddingTop: 30,
+          paddingLeft: 50,
+          paddingRight: 50,
+          paddingBottom: 50,
+          width: '100%',
+          maxWidth: '100%',
+          margin: 'auto',
+          marginTop: 60,
+        }
+    },
+    textFieldStyles: {
+        root: {
+            width:'100%',
+        }
+    }
+}
+
+const classNames = mergeStyleSets({
+    pivot: {
+        margin: 'auto',
+    }
+    
+});
+
+
+
+
+const faq  = () =>   (
+
+    <div id="faq">
+            <div className="ms-Grid-row" style={{paddingBottom:'100px'}}>
+                <div className="ms-Grid-row">
+                    <HeaderHomePage/>
+                </div>
+                    
+                    {/*<Card.Section>
+                        <div style={{
+                                paddingLeft:'80px',
+                                paddingTop:"5px",
+                                paddingBottom:"10px",
+                                marginTop:'40px',
+                                color:'white'
+                        }}>
+                            <Text variant='xxLarge'>
+                                Frequently Asked Questions (FAQs)
+                            </Text>
+                        </div>
+                    </Card.Section>*/}
+                
+                <div className={`s-Grid-col ms-sm9 ms-xl9 ${classNames.pivot}`}>
+                    <Card styles={styles.cardStyles}>
+                        <Card.Section>
+                            <Text 
+                                variant='xLarge'>
+                                    What is the duration of the process?
+                            </Text>
+                        </Card.Section>
+                        <Card.Section>
+                        <Text 
+                            variant='medium'>
+                                The process is of 90 days.
+                            </Text>
+                        </Card.Section>
+                    </Card>
+
+                    <Card styles={styles.cardStyles}>
+                        <Card.Section>
+                            <Text 
+                                variant='xLarge'>
+                                    Who can tell me the documents required for licensing?
+                            </Text>
+                        </Card.Section>
+                        <Card.Section>
+                        <Text 
+                            variant='medium'>
+                                You can visit <a href="http://dgca.nic.in/manuals/ProceMan_Aero.pdf">here</a> to get all the process details.
+                            </Text>
+                        </Card.Section>
+                    </Card>
+
+                    <Card styles={styles.cardStyles}>
+                        <Card.Section>
+                            <Text 
+                                variant='xLarge'>
+                                    Do we provide a license for both public and private use?
+                            </Text>
+                        </Card.Section>
+                        <Card.Section>
+                        <Text 
+                            variant='medium'>
+                                Yes.
+                            </Text>
+                        </Card.Section>
+                    </Card>
+
+                    <Card styles={styles.cardStyles}>
+                        <Card.Section>
+                            <Text 
+                                variant='xLarge'>
+                                    Who will inspect us onsite?
+                            </Text>
+                        </Card.Section>
+                        <Card.Section>
+                        <Text 
+                            variant='medium'>
+                                Director General of Civil Aviation will assign an Aerodrome Inspector with his team, who will be responsible for onsite inspection.
+                            </Text>
+                        </Card.Section>
+                    </Card>
+
+                    <Card styles={styles.cardStyles}>
+                        <Card.Section>
+                            <Text 
+                                variant='xLarge'>
+                                    Who will provide us a license?
+                            </Text>
+                        </Card.Section>
+                        <Card.Section>
+                        <Text 
+                            variant='medium'>
+                              Once DOAS recommend DGCA for grant of a license, DOAS generate license number and update license register, then an online license is generated.
+                            </Text>
+                        </Card.Section>
+                    </Card>
+
+                    <Card styles={styles.cardStyles}>
+                        <Card.Section>
+                            <Text 
+                                variant='xLarge'>
+                                    What if our application for new application is rejected at any phase?
+                            </Text>
+                        </Card.Section>
+                        <Card.Section>
+                        <Text 
+                            variant='medium'>
+                              The Aerodrome Inspector will recommend changes and action plan.
+                            </Text>
+                        </Card.Section>
+                    </Card>
+
+                    <Card styles={styles.cardStyles}>
+                        <Card.Section>
+                            <Text 
+                                variant='xLarge'>
+                                    After how many days we have to renew the license?
+                            </Text>
+                        </Card.Section>
+                        <Card.Section>
+                        <Text 
+                            variant='medium'>
+                              The license should be renewed in every two years.
+                            </Text>
+                        </Card.Section>
+                    </Card>
+                    
+                </div>
+            </div>
+    </div>
+            
+        )
+    
+export default faq
