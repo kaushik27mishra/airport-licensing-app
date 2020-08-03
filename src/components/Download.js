481b4dfc0e0165174e3822b3af01948198e9a276
Shritesh <shritesh.sj@gmail.com>
2020-08-03 13:46:21 +0530

    Download button working

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useLazyQuery } from '@apollo/react-hooks';
 import { gql } from '@apollo/react-hooks';
+import axios from "axios";
+
+const file = async (url) => {
+    const data = await axios.get(url);
+    return data.data;
+};
 
 function Download(props) {
     const [generateLicense, { loading, data }] = useLazyQuery(LICENSE);
+    const [download, setDownload] = useState(null);
+    useEffect(()=> {
+        if(data){
+            (async () => setDownload(await file(data.generateLicense.license)))();
+        }
+    }, [data]);
+
+    
     if(loading)
         return 'loading'
 
-    generateLicense({ variables: {id: props.id,year: 2030} })
-
-    if(data) {
-        
+    if(download){
+        return (<a download="Doc.pdf" href={`data:application/pdf;base64,${download}`}>Image</a>);
     }
-    
+    if(data)console.log(data);
     return (
         <div className="ms-Grid-row">
             <button onClick={() => {
-                
-
+                generateLicense({ variables: {id: props.id,year: 20} })
             }}>
                 Download now
             </button>     
